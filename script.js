77415af00cb4a4e119c3908644302fd1afe4b380
riwel13 <karina.velmushkina@gmail.com>
2026-01-08 19:27:20 +0100

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-const colorBtnsEl = document.querySelectorAll("input[name='color']")
-const bodyEl = document.querySelector("body")
+const colorBtnsEl = document.querySelectorAll("input[name='color']");
+const bodyEl = document.querySelector("body");
 
-colorBtnsEl.forEach((btn) => {btn.addEventListener("change", () => bodyEl.style.backgroundColor = btn.value)})
+colorBtnsEl.forEach((btn) => {
+  btn.addEventListener(
+    "change",
+    () => (bodyEl.style.backgroundColor = btn.value)
+  );
+});
 
 //
 
-const nameEl = document.getElementById("name-input")
-const h1El = document.querySelector("h1")
-nameEl.addEventListener("input", () =>
-     {h1El.textContent = nameEl.value
-    if (nameEl.value == "") {
-        h1El.textContent = "незнайомець"
+const nameEl = document.getElementById("name-input");
+const h1El = document.querySelector("h1");
+nameEl.addEventListener("input", () => {
+  h1El.textContent = nameEl.value;
+  if (nameEl.value == "") {
+    h1El.textContent = "незнайомець";
+  }
+});
 
-    }
+const validEl = document.getElementById("validation-input");
+
+validEl.addEventListener("blur", () => {
+  if (validEl.value.length >= validEl.dataset.length) {
+    validEl.classList.add("valid");
+    validEl.classList.remove("invalid");
+  } else {
+    validEl.classList.add("invalid");
+    validEl.classList.remove("valid");
+  }
+});
+
+
+//
+
+const imputEl = document.querySelector("#font-size-control")
+const textEl = document.querySelector("#text")
+
+imputEl.addEventListener("input", () => {
+    textEl.style.fontSize = imputEl.value + px
 })
